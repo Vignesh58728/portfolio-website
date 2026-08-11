75ed043cba53a8bef38a9242815b2da7680f6540
Vignesh58728 <saivigneshas@gmail.com>
2026-08-12 00:23:40 +0530

    Fix TypeScript path alias imports for utils module

--- src/components/ui/shooting-stars.tsx
+++ src/components/ui/shooting-stars.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { cn } from "../../lib/utils";
+import { cn } from "@/lib/utils";
 import { useEffect, useState, useRef } from "react";
 
 interface ShootingStar {
--- src/components/ui/stars-background.tsx
+++ src/components/ui/stars-background.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState, useEffect, useRef, RefObject, useCallback } from "react";
-import { cn } from "../../lib/utils";
+import { cn } from "@/lib/utils";
 
 interface StarProps {
    x: number;
